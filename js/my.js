1003d5477c7fbbcf11b4e384ef9c6e1bda0e9ed7
Naman rai <namanrai309@gmail.com>
2022-06-18 17:48:25 +0530

    v8

--- js/my.js
+++ js/my.js
@@ -5,55 +5,141 @@ const ani1 = document.getElementById("hide");
 const ani2 = document.getElementById("nav-toggle-btn");
 const ani3 = document.getElementById("brand-name");
 const remo = document.getElementById("hide1");
+const remo2 = document.getElementById("hide2");
+const navi = document.querySelectorAll("nav-con");
+
+// var cur_width = window.innerWidth;
+// if (cur_width > 1024 && cur_width > 640) {
+    
+// console.log("b");
+//     // location.reload();
+ 
+// } 
+
+// console.log(cur_width);
+
+// var width = window.innerWidth;
+
+// window.resizeTo(400) = function() {
+
+
+// }
+
+// window.onresize(function () {
+//     location.reload();
+// });
+
+// window.onresize = function() {
+
+//     window.setTimeout(function () {
+//             location.reload();
+//         },1000);
+//     }
+
+
+//     remo.remove();
+
+//     location.reload()= function() {
+//     if(w <=800){
+//        
+
+//     }
+// }
+
+
+// if(width> 769){
+//     window.setTimeout(function () {
+//         location.reload();
+//     },1000);
+// }else{
+//     if(width < 769){
+//     window.setTimeout(function () {
+//         location.reload();
+//     },1000);
+// }}
 
 
 
-document.querySelector(function () {
-    var width = document.querySelector(window).width();
-    var screen = "";
-    if (width < 1024 && width > 640) {
-        screen = "small";
-    } else if (width > 1024) { screen = "big"; }
-    document.querySelector(window).onresize(function () {
-        var cur_width = document.querySelector(window).width();
+    window.onresize=function () {
+        var width = window.innerWidth;
 
-        if (cur_width < 1024 && cur_width > 640 && screen == "big") {
+        if (width >1024 )  {
             location.reload();
-            remo.add();
-        } else if (cur_width > 1024 && screen == "small") {
+            remo2.remove();
+         
+        } else if (width  < 767) {
             location.reload();
             remo.remove();
+
         }
        
-    })
-});
+    };
+
+
+
+// window.addEventListener('resize', function () {
+//     "use strict";
+//     window.location.reload();
+// })
 
 
 
 
- window.onscroll = function scrollfun() {
-    if(document.body.scrollTop >500 || document.documentElement.scrollTop >500){
+window.onscroll = function scrollfun() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
         mybutton.style.opacity = "1";
         dow.classList.add("sha-dow");
         ani1.classList.add("ani1");
         ani2.classList.add("ani2");
         ani3.classList.add("ani3");
-    }else{
+    } else {
         mybutton.style.opacity = "0";
         dow.classList.remove("sha-dow");
         ani1.classList.remove("ani1");
         ani2.classList.remove("ani2");
         ani3.classList.remove("ani3");
-        
+
     }
- }
+  
+    // var io = new IntersectionObserver(entries => {
+    //     for (const entry of entries){
+    //         if (window.innerWidth > 1024)  {
+    //             if ((entries[0].intersectionRatio>0.86)) {
+    //                 document.getElementById("sha").style.backgroundColor = "#27272e";
+    //                 dow.classList.remove("sha-dow");
+                    
+                
+            
+    //             } else if((!entry.isIntersecting) || (entries[0].intersectionRatio<0.8)) {
+    //                 dow.classList.add("sha-dow");
+    //                 document.getElementById("sha").style.backgroundColor = "#ffffff";
+    //             }
+             
+    //         } else if (window.innerWidth < 700) {
+    //             if ((entries[0].intersectionRatio>0.6)) {
+    //                 document.getElementById("sha").style.backgroundColor = "#27272e";
+    //                 dow.classList.remove("sha-dow");
+    //             } else if((!entry.isIntersecting) || (entries[0].intersectionRatio<0.8)) {
+    //                 dow.classList.add("sha-dow");
+    //                 document.getElementById("sha").style.backgroundColor = "#ffffff";
+    //             }
+    
+    //         }
+
+    //     }
+            
+    //     // console.log(`$(entry.target.id) is in view: ${entry.isIntersecting}`);
+    //     // console.log(entries[0].intersectionRatio);
+    // })
+    // io.observe(document.querySelector("#anchor1"));
+}
 
 
- function topfunction() {
+function topfunction() {
     // document.body.scrollTop = 0,'smooth';
     // document.documentElement.scrollTop = 0,'smooth';
-    window.scrollTo({top: 0, behavior: "smooth"});
- }
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
 
 
 //  if (
@@ -73,24 +159,12 @@ document.querySelector(function () {
 // }
 
 
-var io = new IntersectionObserver(entries =>{
-    for(const entry of entries) 
-    if ((entry.isIntersecting)) {
-        document.getElementById("h-scroll").classList.remove("fadeInleft");
-        document.getElementById("h-scroll").classList.add("fadeInright");
-    }else{
-        document.getElementById("h-scroll").classList.remove("fadeInright");
-        document.getElementById("h-scroll").classList.add("fadeInleft");
-        
-    }
-    // console.log(`$(entry.target.id) is in view: ${entry.isIntersecting}`);
-    // console.log(entries[0].intersectionRatio);
-})
 
 
 
 
-io.observe(document.querySelector("#anchor1"));
+
+
 
 
 
